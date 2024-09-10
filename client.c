859b33d3620b77462865f03010d28fabefff889d
Leonardo Gueno Rissetto <leonardorissetto01@gmail.com>
2024-09-10 15:54:29 -0300

    teste

--- client.c
+++ client.c
@@ -8,16 +8,16 @@
 #define PORT 5566
 #define BUFFER_SIZE 1024
 
-// Função para limpar a linha anterior no terminal
+char buffer_send[BUFFER_SIZE];
+
 void clear_line()
 {
-    printf("\033[A"); // Move o cursor para a linha de cima
-    printf("\033[K"); // Limpa a linha atual
+    printf("\033[A");
+    printf("\033[K");
 }
 
 void clear_screen()
 {
-    // Limpa a tela do terminal
     printf("\033[H\033[J");
 }
 
@@ -32,7 +32,7 @@ void *receive_messages(void *sock)
         buffer[n] = '\0';
         clear_line();
         printf("\n%s\n", buffer);
-        printf("\n> "); // Reexibe o prompt após uma mensagem ser recebida
+        printf("\n> ");
         fflush(stdout);
     }
 
@@ -43,7 +43,6 @@ int main()
 {
     int sock;
     struct sockaddr_in addr;
-    char buffer[BUFFER_SIZE];
     pthread_t tid;
     char name[50];
 
@@ -67,30 +66,26 @@ int main()
     }
     printf("\n[+]Connected to the Server.\n");
 
-    // Ask for the client's name
     printf("\nEnter your name: ");
     fgets(name, 50, stdin);
-    name[strcspn(name, "\n")] = '\0'; // Remove newline character
+    name[strcspn(name, "\n")] = '\0';
     send(sock, name, strlen(name), 0);
 
-    clear_screen(); // Limpa a tela do terminal após a inserção do nome
+    clear_screen();
 
-    // Display a welcome message
     printf("Welcome to the chat, %s!\n\n", name);
-    printf("> "); // Mostra o prompt de entrada de mensagem
+    printf("> ");
     fflush(stdout);
 
-    // Create a thread to receive messages from the server
     pthread_create(&tid, NULL, receive_messages, &sock);
 
     while (1)
     {
-        bzero(buffer, BUFFER_SIZE);
-        fgets(buffer, BUFFER_SIZE, stdin);
-        buffer[strcspn(buffer, "\n")] = '\0'; // Remove newline character
-        send(sock, buffer, strlen(buffer), 0);
+        bzero(buffer_send, BUFFER_SIZE);
+        fgets(buffer_send, BUFFER_SIZE, stdin);
+        buffer_send[strcspn(buffer_send, "\n")] = '\0';
+        send(sock, buffer_send, strlen(buffer_send), 0);
         clear_line();
-        printf("> "); // Reexibe o prompt após enviar uma mensagem
         fflush(stdout);
     }
 
